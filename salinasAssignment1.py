76648f1d5ab40a388dc7fd714e485890e6dda50d
Gibbtronimo <salinas.ryan00@gmail.com>
2022-01-30 13:06:26 -0600

    Add Assignment 1 python script with problem 1

---
+++ salinasAssignment1.py
@@ -0,0 +1,13 @@
+"""
+	Assignment 1
+	Ryan Salinas
+	COSC 2348.001
+	1/27/22
+"""
+
+print("This is assignment 1\n")
+#Problem 1: Personal Information
+#	    - print out personal information to console
+print("Name:    Ryan Salinas")
+print("Address: 1109 Nancy Dr, Corpus Christi")
+print("         Texas, 78412")
