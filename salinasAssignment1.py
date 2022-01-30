2e10f2ed6c2282cb1602c827d6bbb7929cb10482
Gibbtronimo <salinas.ryan00@gmail.com>
2022-01-30 13:48:49 -0600

    Update git

--- salinasAssignment1.py
+++ salinasAssignment1.py
@@ -10,9 +10,12 @@ print("Problem 1:\n")
 #	    - print out personal information to console
 print("Name:    Ryan Salinas")
 print("Address: 1109 Nancy Dr, Corpus Christi")
-print("         Texas, 78412")
+print("         Texas, 78412\n")
 
 print("Problem 2:\n")
 #Problem 2: Land Calculation
 #	    - calculates acres from square feet
 sqFeet = int(input("Enter the sqare footage to convert to acres: ")
+acres = sqFeet/43560
+
+print(acres)
