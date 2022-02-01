8284fa3c6cdac0fdaad4560dc26d0a2c4f843258
Gibbtronimo <salinas.ryan00@gmail.com>
2022-02-01 13:28:48 -0600

    Upload problem 3

--- salinasAssignment1.py
+++ salinasAssignment1.py
@@ -17,5 +17,12 @@ print("\nProblem 2:\n")
 #	    - calculates acres from square feet
 sqFeet = float(input("Enter the sqare footage to convert to acres: "))
 acres = float(sqFeet/43560)
-
 print("You have approximately {:.2f} acres." .format(acres))
+
+print("\nProblem 3:\n")
+# Problem 3: Car Distance
+#	     - calculates distance traveled at certain speed and time
+carSpeed = 70
+print("The car will travel {:d} miles in 6 hours" .format(carSpeed*6))
+print("The car will travel {:d} miles in 10 hours" .format(carSpeed*10))
+print("The car will travel {:d} miles in 15 hours" .format(carSpeed*15))
