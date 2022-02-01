d0e251421379a9acc81ed8a2cbb05d509c10da95
Gibbtronimo <salinas.ryan00@gmail.com>
2022-02-01 16:09:49 -0600

    Update problem 6 to have proper output of leap year and dates

--- salinasAssignment1.py
+++ salinasAssignment1.py
@@ -63,14 +63,14 @@ print("\nProblem 6:\n")
 year = int(input("Enter a year: "))
 if year % 100 == 0:
 	if year % 400 == 0:
-		print("Leap year")
+		print("This leap year has 29 days in February")
 	else:
-		print("Not leap year")
+		print("Not a leap year so there are 28 days in February")
 else:
 	if year % 4 == 0:
-		print("Leap year")
+		print("This leap year has 29 days in February")
 	else:
-		print("Not leap year")
+		print("Not a leap year so there are 28 days in February")
 
 print("\nProblem 7:\n")
 # Problem 7: BMI Calculator
