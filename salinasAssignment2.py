109b839cb5b8ad5ee77b0647a174a71b9c163116
Gibbtronimo <salinas.ryan00@gmail.com>
2022-02-08 00:47:09 -0600

    Upload problem 1 to Assignment 2

--- salinasAssignment2.py
+++ salinasAssignment2.py
@@ -5,3 +5,14 @@
     2/8/22
 '''
 
+print("Problem 1:")
+# Problem 1: Pattern Printing
+#            - print out a series of asterisks using a loop
+# part i
+num = 6
+for a in range(num):
+    print(a*"*")
+# part ii
+for b in reversed(range(num)):
+    print(b*" " + (num-b)*"*")
+
