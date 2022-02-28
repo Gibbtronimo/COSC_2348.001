3b29fa7ff975145941221f7dd9057da4d764538f
Gibbtronimo <salinas.ryan00@gmail.com>
2022-02-28 02:27:01 -0600

    Add problem 2 to assignment 5

--- salinasAssignment5.py
+++ salinasAssignment5.py
@@ -6,6 +6,7 @@
 # Problem 1: More Code String Converter
 #            - a morse code dictionary is used to convert a user-input string into morse code
 
+# Morse Code Dictionary for problem 1
 morseTable = {' ':' ',',':'--..--','.':'.-.-.-','?':'..--..',
               '0':'-----','1':'.----','2':'..---','3':'...--','4':'....-',
               '5':'.....','6':'-....','7':'--...','8':'---..','9':'----.',
@@ -20,6 +21,38 @@ def morseCode():
     for ch in userString:
         print(morseTable[ch],end='')
 
+# Problem 2: String Analysis
+#            - these functions count the number of vowels and consonants in a user-input string
+
+# Vowel list used in both functions
+vList = ['a','e','i','o','u']
+
+# Function to count vowels
+def strVowels(strInput):
+    strInput = strInput.lower()
+    vowels = 0
+    for ch in strInput:
+        if ch in vList:
+            vowels += 1
+    return vowels
+
+# Function to count consonants
+def strConsts(strInput):
+    strInput = strInput.lower()
+    consts = 0
+    for ch in strInput:
+        if (ch not in vList) and ch.isalpha():
+            consts += 1
+    return consts
+
+
 if __name__ == "__main__":
     print("Problem 1:\n")
     morseCode()
+
+    print("\nProblem 2:\n")
+    strAnalysis = input("Enter a string: ")
+    vnum = strVowels(strAnalysis)
+    cnum = strConsts(strAnalysis)
+    print("There are",vnum,"vowels")
+    print("There are",cnum,"consonants")
