239e884fb3841137989b98f2e9652f9c869536ef
Gibbtronimo <salinas.ryan00@gmail.com>
2022-03-31 11:56:06 -0500

    Assignment 7, Problem 2

--- salinasAssignment7_bash.sh
+++ salinasAssignment7_bash.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 # Uses bash shell
 echo This is Assignment 7
+
+var_name="Ryan"
+class_1="Intro to Database Systems"
+class_2="Cyber Defense II"
+class_3="Intro to Scripting"
+class_4="Technical and Professional Writing"
+class_5="Digital Forensics"
+
+echo $var_name
+echo $class_1
+echo $class_2
+echo $class_3
+echo $class_4
+echo $class_5
