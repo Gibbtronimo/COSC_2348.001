476e7bbe74a6c05618a92daa5e05849716e802c2
Gibbtronimo <salinas.ryan00@gmail.com>
2022-04-12 22:50:36 -0500

    Assignment 8, Problem 4

--- salinasAssignment8.sh
+++ salinasAssignment8.sh
@@ -56,3 +56,20 @@ do
 done
 echo Sum: $sum
 
+#Problem 4
+echo -e "\nProblem 4:"
+echo -n "Enter a city: "
+read input
+city=${input,,}
+case "$city" in
+	#case 1
+	"corpus") echo "Texas A&M University Corpus Christi"
+	;;
+	#case 2
+	"kingsville") echo "Texas A&M University Kingsville"
+	;;
+	#case 3
+	"commerce") echo "Texas A&M University Commerce"
+	;;
+*) echo "Texas A&M University"
+esac
