6d86e5a35b8467aa1a75812e82b109f33847abff
Gibbtronimo <salinas.ryan00@gmail.com>
2022-04-17 17:11:14 -0500

    Assignment 9, Problem 4: comment out all of it

--- salinasAssignment9.sh
+++ salinasAssignment9.sh
@@ -66,17 +66,34 @@ echo ${arr3[*]}
 
 #Problem 4
 # defining the function to find prime numbers in an array
-function find_primes {
-	if [ $# -eq 1 ]; then
-		local -n list=$1
-		for num in ${list[*]}; do
-			echo $num
-		done
-	else
-		echo This function only takes one argument - an array of numbers
-	fi
-
-}
+#function find_primes {
+#	i=2
+#	f=0
+#	if [ $# -eq 1 ]; then
+#		local -n list=$1
+#		for num in ${list[*]}; do
+#			if test $num -eq 1 || test $num -eq 2; then
+#				break
+#
+#			fi
+#
+#			while test $i -le 'expr $num / 2'; do
+#				if test 'expr $num % $i' -eq 0; then
+#					$f=1
+#				fi
+#				$i='expr $i + 1'
+#			done
+#			if test $f -eq 1; then
+#				continue
+#			else
+#				echo -n $num" "
+#			fi
+#		done
+#	else
+#		echo This function only takes one argument - an array of numbers
+#	fi
+#
+#}
 # calling the function and passing arr3 as an argument
-echo
-find_primes arr3
+#echo
+#find_primes arr3
