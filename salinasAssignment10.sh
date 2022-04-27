3d33b56ae8fbb5ce7fa8469e1a64c87db0b21fd8
Gibbtronimo <salinas.ryan00@gmail.com>
2022-04-26 23:54:42 -0500

    Assignment 10, Problem 2 not working as well

--- salinasAssignment10.sh
+++ salinasAssignment10.sh
@@ -8,23 +8,38 @@ com
 
 #Problem 1
 echo Problem 1:
-sum1="A10nums.txt"
-declare -a num_array
-while read line; do
-	echo $line
-	num_array+=($line)
-done < $sum1
-echo ${num_array[@]}
+#sum1="A10nums.txt"
+#declare -a num_array
+#while read line; do
+#	echo $line
+#	num_array+=($line)
+#done < $sum1
+#echo ${num_array[@]}
+#
+#function recursive_add () {
+#	local file=$1
+#	local n=$2
+#	global sum=$((file[n]))
+#	if (( n == 0 )); then
+#		echo $n
+#		return
+#	fi
+#	$FUNCNAME $file $((n-1)) $((sum+n))
+#}
+#
+#recursive_add $sum1 5
 
-function recursive_add () {
-	local file=$1
-	local n=$2
-	global sum=$((file[n]))
-	if (( n == 0 )); then
-		echo $n
-		return
+#Problem 2
+echo -e "\nProblem 2:\n"
+
+function fibonacci () {
+	n=$1
+	if [ $n -le 1 ]; then
+		return $n
+	else
+		return $(( fibonacci ( n - 1 ) + fibonacci ( n - 2 ) ))
 	fi
-	$FUNCNAME $file $((n-1)) $((sum+n))
 }
 
-recursive_add $sum1 5
+read -p "Enter a number >= 2: " num
+fibonacci $num
